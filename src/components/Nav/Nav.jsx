9b195fa26829cc71c0aca722b7d794bf4c59a8e9
elvedin123 <elvedinc97@gmail.com>
2023-06-12 15:06:28 -0400

    added major styling to lamnding page

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -2,13 +2,15 @@ import './Nav.css'
 const Nav = () => {
 
   return (
-    <nav className="nav-container">
+    <div className="nav-container">
       <div className='links-container'>
         <a href="#about">About</a>
         <a href="#contact">Contact</a>
-
+        <a href="#services">Services</a>
       </div>
-    </nav>
+
+
+    </div>
   )
 }
 
