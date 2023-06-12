8bae0cf02ddbd6580f517e69864781e2d734d7a6
elvedin123 <elvedinc97@gmail.com>
2023-06-12 14:22:18 -0400

    working on landing page design

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -3,7 +3,6 @@ const Nav = () => {
 
   return (
     <nav className="nav-container">
-      <a href="#home" className='brand-name'> EC Soccer</a>
       <div className='links-container'>
         <a href="#about">About</a>
         <a href="#contact">Contact</a>
--- src/screens/landingpage.jsx
+++ src/screens/landingpage.jsx
@@ -1,9 +1,18 @@
 import Nav from "../components/Nav/Nav.jsx"
-
+import './landingpage.css'
+import React from "react"
+import logo from '../assets/soccer-player.png'
 const LandingPage = () => {
   return (
-    <div>
-      <Nav />
+    <div className="landingpage-container">
+      <div className="ec-soccer">
+        <h1 className='title'>EC Soccer</h1>
+        <img className="soccer-image" src={logo} alt="soccerball" />
+      </div>
+      <div className="app-content">
+        <Nav />
+      </div>
+
     </div>
   )
 }
